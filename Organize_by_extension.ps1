10da518261eb3a2639b7e5ebb91cbd01b5e46df3
zarbam <Zarbam@gmail.com>
2022-12-08 14:51:20 -0500

    Create Organize_by_extension.ps1

---
+++ Organize_by_extension.ps1
@@ -0,0 +1,20 @@
+# Set the path to the folder where the files are located
+$folderPath = "C:\users\zarbam\downloads"
+
+# Get all of the files in the folder
+$files = Get-ChildItem $folderPath
+
+# Loop through each file
+foreach ($file in $files) {
+  # Get the extension of the file
+  $ext = $file.Extension
+
+  # Check if a folder with this extension already exists
+  if (-not (Test-Path "$folderPath\$ext")) {
+    # If not, create a new folder with this name
+    New-Item -ItemType Directory -Path "$folderPath\$ext"
+  }
+
+  # Move the file to the folder with the same extension
+  Move-Item $file.FullName "$folderPath\$ext\"
+}
